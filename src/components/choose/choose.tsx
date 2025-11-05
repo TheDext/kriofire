d19d91c6aeb503bcd8ef794394f469508b537440
kirill <kirill.korev.mev@gmail.com>
2025-11-06 02:28:18 +0300

    добавил ссылку на журнал

--- src/components/choose/choose.tsx
+++ src/components/choose/choose.tsx
@@ -3,6 +3,7 @@ import classes from './choose.module.scss';
 import chooseConfig from '@config/choose.config';
 import classNames from '@/utils/classNames';
 import { useInView } from 'react-intersection-observer';
+import {Link} from "react-router";
 
 export const Choose = () => {
     const { images, subtitle } = chooseConfig;
@@ -20,18 +21,30 @@ export const Choose = () => {
                     ref={ref}
                     className={classNames(
                         classes.label,
-                        { [classes._inView]: inView },
+                        {[classes._inView]: inView},
                         []
                     )}
                 >
                     За <span>качество, сроки</span> и
                     <span> умение решать сложные</span> задачи.
                 </div>
+
+                <Link
+                    className={classNames(
+                        classes.link,
+                        {[classes._inView]: inView},
+                        []
+                    )}
+                    ref={ref}
+                    to="https://www.calameo.com/read/00486243629a49bde3277?page=27"
+                >
+                    Читайте о нас в журнале Экономическая стратегия, 2025
+                </Link>
                 <div
                     ref={ref}
                     className={classNames(
                         classes.row,
-                        { [classes._inView]: inView },
+                        {[classes._inView]: inView},
                         []
                     )}
                 >
@@ -39,7 +52,7 @@ export const Choose = () => {
                         <div key={index} className={classes.column}>
                             <div className={classes.item}>
                                 <div className={classes.img}>
-                                    <img src={img} alt="img" />
+                                    <img src={img} alt="img"/>
                                 </div>
                             </div>
                         </div>
